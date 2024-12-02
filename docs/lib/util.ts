02167ef4fd58516b74acf2523492a0315399100d
Bill Mill <bill@billmill.org>
2024-12-01 22:15:12 -0500

    feat(utils): throw an error if a field is undefined

--- docs/lib/util.ts
+++ docs/lib/util.ts
@@ -1,5 +1,8 @@
 export function sliceQuantile(arr: any[], field: string, percentage: number) {
+  if (!Object.hasOwn(arr[0], field)) {
+    throw new Error(`undefined field ${field}: ${arr[0]}`)
+  }
   return arr
     .sort((a, b) => a[field] - b[field])
-    .slice(Math.ceil(arr.length * percentage));
+    .slice(Math.ceil(arr.length * percentage))
 }
