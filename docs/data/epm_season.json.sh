077d1c81ec1328b4abb19ad524e64ea8776a9163
Bill Mill <bill@billmill.org>
2025-05-05 09:57:15 -0400

    fix: epm_season data script

--- docs/data/epm_season.json.sh
+++ docs/data/epm_season.json.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-curl -s https://dunksandthrees.com/epm/actual | grep 'const data' > /tmp/epmSeason.js
-printf "\nconsole.log(JSON.stringify(data));" >> /tmp/epmSeason.js
-node /tmp/epmSeason.js
+JS=/tmp/epmSeason.js
+printf "const data = " > $JS
+# grab the javascript-formatted object from the source
+curl -s https://dunksandthrees.com/epm | sed -n 's/.*data: \(\[.*\]\).*/\1/p' >> $JS
+printf "\nconsole.log(JSON.stringify(data));" >> $JS
+node $JS
